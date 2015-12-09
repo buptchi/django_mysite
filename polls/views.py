1a5f71d468d14d5ce8ecdba152207e261865fca2
buptchi <buptcqz@qq.com>
2015-12-10 00:09:58 +0800

    1210from suse django part5 ///ver2.0

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
+'''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -33,6 +33,7 @@ def detail(request, question_id):
 def results(request, question_id): 
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html',{'question':question})
+'''
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
